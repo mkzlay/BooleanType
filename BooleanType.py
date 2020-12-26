8acf3e6a6200da71d925681263c4f2f73241b485
Mustafa Kaan Kızılay <75048064+mkzlay@users.noreply.github.com>
2020-12-27 01:47:03 +0300

    Add files via upload

---
+++ BooleanType.py
@@ -0,0 +1,32 @@
+Python 3.8.6 (tags/v3.8.6:db45529, Sep 23 2020, 15:52:53) [MSC v.1927 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> #The Python Boolean type is one of Pythons built-in data types. C mon, lets show it.
+>>> a = "Python"
+>>> b = "Python"
+>>> a
+'Python'
+>>> b
+'Python'
+>>> a is b
+True
+>>> a==b
+True
+>>> a = (1,2,3)
+>>> b = (1,2,3)
+>>> a
+(1, 2, 3)
+>>> b
+(1, 2, 3)
+>>> a is b
+False
+>>> a==b
+True
+>>> type(True)
+<class 'bool'>
+>>> type(False)
+<class 'bool'>
+>>> int(True)
+1
+>>> int(False)
+0
+>>> #While Python has several numeric types, there is only one logical type: booleans. A boolean can only take 2 values: True or False. And this is all you need, Ifyou are logical...
